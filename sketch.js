5105e00573cbc61c1a1d3cdaee2ef87a45f700e8
Yzerrr <joeyyzer@gmail.com>
2021-05-17 20:41:33 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
 /* 👇 Start writing your p5.js code here */
+let img;
+let cnv;
+function preload(){
+  img = loadImage('assets/board.jpg');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  cnv = createCanvas(img.width, img.height);
+  // print(img.width, img.height)
+  let newCanvasX = (windowWidth - img.width)/2;
+  let newCanvasY = (windowHeight - img.height)/2;
+  cnv.position(newCanvasX, newCanvasY);
+  
+for(let col = 0; col < img.width; col+=10){
+  for(let row = 0; row < img.height; row+=10){
+    let xPos = col;
+    let yPos = row;
+    let c = img.get(xPos,yPos);
+    push();
+    translate(xPos, yPos);
+    
+    
+    
+    rect(col,row,10,5);
+    pop();
+  }
+}
 }
 
-function draw() {
-  background(220);
-  ellipse(width / 2, height / 2, 50, 50);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
